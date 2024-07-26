edc08097b713a108245d1860356eb6ea120f7ee0
TAO <tao.yang@verystar.cn>
2024-07-26 16:02:40 +0800

    feat: change

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+/*
+ * @LastEditors: Tao Yang
+ * @Description: 暂无描述
+ * @FilePath: /nextjs-dashboard/app/dashboard/invoices/page.tsx
+ * @Date: 2024-07-26 15:26:04
+ * @LastEditTime: 2024-07-26 15:26:05
+ * @Author: Tao Yang
+ */
+
+export default function Page() {
+  return <p>Invoices Page</p>;
+}
